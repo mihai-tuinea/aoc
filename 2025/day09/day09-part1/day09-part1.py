0666aeca6d54a3e5ff2b1d30bdc96242cbe62230
Mihai Tuinea <mihai.tuinea27@gmail.com>
2025-12-09 17:17:57 +0200

    day 9

---
+++ 2025/day09/day09-part1/day09-part1.py
@@ -0,0 +1,28 @@
+tiles: list[tuple] = []  # (col, row)
+with open("input.txt", "r") as file:
+    for line in file:
+        line = line.strip()
+        col, row = line.split(",")
+        tiles.append((int(col), int(row)))
+
+num_tiles = len(tiles)
+
+result = 0
+for i in range(0, num_tiles):
+    for j in range(i + 1, num_tiles):
+        t1 = tiles[i]
+        t2 = tiles[j]
+
+        min_x = min(t1[0], t2[0])
+        max_x = max(t1[0], t2[0])
+        min_y = min(t1[1], t2[1])
+        max_y = max(t1[1], t2[1])
+
+        width = max_x - min_x + 1
+        height = max_y - min_y + 1
+        area = width * height
+
+        if area > result:
+            result = area
+
+print(result)
